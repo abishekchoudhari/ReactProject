7e541efa0fa05e9fe5fd5482d02bdbc3cd48eefc
Abhishek <abishek.choudhari555@gmail.com>
2022-05-06 15:32:41 +0530

    Added codemagic.yaml file

---
+++ script.bat
@@ -0,0 +1,6 @@
+git init -b main
+git add .
+git commit -m "Added codemagic.yaml file"
+git remote add origin https://github.com/abishekchoudhari/ReactProject.git 
+git remote -v
+git push origin main
